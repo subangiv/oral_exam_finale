ec953b6ffcb06f9c2130727586b1481cc81d98bd
Subangi <suba0107@stud.kea.dk>
2020-11-16 21:36:57 +0100

    initializing git repo - scaffolding + scss-partials with variables and different components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="primary-text">Hola</h1>
+        <button className="btn rounded btn-primary">Button</button>
+        <button className="btn rounded btn-primary outlined">Button</button>
+        <button className="btn rounded btn-donate">Button</button>
     </div>
   );
 }
