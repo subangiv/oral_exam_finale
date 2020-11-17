f7e05f2e64dbbc4121c48a201aaeb12776d18d41
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-11-17 16:52:49 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <h1 className="primary-text">Hola</h1>
-        <button className="btn rounded btn-primary">Button</button>
-        <button className="btn rounded btn-primary outlined">Button</button>
-        <button className="btn rounded btn-donate">Button</button>
+      <h1 className="primary-text">Hej</h1>
+      <button className="btn rounded btn-primary">Button</button>
+      <button className="btn rounded btn-primary outlined">Button</button>
+      <button className="btn rounded btn-donate">Button</button>
     </div>
   );
 }
