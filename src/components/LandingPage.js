98cc10028c9ae0750fc77e043250246e0402aa10
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-11-20 19:21:35 +0100

    added Intro section for landingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,14 @@
 import React from "react";
-import styles from "./LandingPage.module.scss";
-import landingpageimg from "../images/landing-page-banner.png";
+import HeroBanner from "./HeroBanner";
+import Intro from "./Intro";
+import StepsProcess from "./StepsProcess";
 
 export default function LandingPage() {
   return (
-    <section className={styles.flex}>
-      <div className={styles.flex__right}>
-        <div className={styles.padding__left__large}>
-          <h1>
-            We bring donors, applicants, and providers of physical products
-            together.
-          </h1>
-          <p>
-            Program started in 2018 and represent effective way to reduce
-            poverty and hunger in Venezuela.
-          </p>
-          <button className="btn rounded btn-secondary">Donate</button>
-        </div>
-      </div>
-      <div className={styles.flex__left}>
-        <img src={landingpageimg} />
-        <div className={styles.purplebox__large}></div>
-      </div>
+    <section>
+      <HeroBanner />
+      <Intro />
+      <StepsProcess />
     </section>
   );
 }
---
+++ src/components/StepsProcess.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./LandingPage.module.scss";
+
+export default function StepsProcess() {
+  return (
+    <section className={styles.flex__column__purple}>
+      <h3>How does it work?</h3>
+    </section>
+  );
+}
