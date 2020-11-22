0a17a901a4238b57a5e16de985e65b0741bf01d7
rachelvo <rach0727@stud.kea.dk>
2020-11-22 16:57:32 +0100

    made Header for mobile version

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,22 @@
 import React from "react";
+import styles from "./Header.module.scss";
+import BurgerMenu from "../burgermenu/BurgerMenu";
 
 export default function Header(props) {
-  console.log(props);
+  return (
+    <header className={styles.header}>
+      <h2 className={styles.polloName}>
+        POLLO<span>POLLO</span>
+      </h2>
+      <div id="actionBtns" className={styles.actionBtns}>
+        <button className="btn rounded btn-donate" children="Donate" />
+        <button className="btn rounded btn-primary" children="Offer product" />
+        <button
+          className="btn rounded btn-primary"
+          children="Apply for product"
+        />
+      </div>
+      <BurgerMenu />
+    </header>
+  );
 }
