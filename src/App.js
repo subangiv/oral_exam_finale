7ef55cefa11cde29d884edfcd08a1ea1aaef8925
rachelvo <rach0727@stud.kea.dk>
2020-11-22 16:56:13 +0100

    imported header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Suspense } from "react";
 import "./App.css";
 // import Footer from "./components/footer/Footer";
+const Footer = React.lazy(() => import("./components/footer/Footer"));
+const Header = React.lazy(() => import("./components/header/Header"));
 function App() {
-  const Footer = React.lazy(() => import("./components/footer/Footer"));
   return (
     <div className="App">
       <Suspense fallback={<p>Loading...</p>}>
+        <Header />
+        &emsp; &emsp;
         <Footer />
       </Suspense>
     </div>
