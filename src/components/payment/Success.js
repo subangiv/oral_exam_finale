a92bd36298270b58954c156044903d96aa98838e
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-11-23 13:56:31 +0100

    edited success page

--- src/components/payment/Success.js
+++ src/components/payment/Success.js
@@ -4,18 +4,22 @@ import SuccessText from "./SuccessText";
 import SuccessImg from "./SuccessImg";
 //import Confetti from "react-dom-confetti";
 import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 
 export default function Success() {
   const colors = ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"];
   const initialVelocityX = 4;
   const initialVelocityY = 20;
   const gravity = 0.3;
+  const { width, height } = useWindowSize();
 
   return (
     <section className={styles.wrapper}>
       <SuccessText />
       <SuccessImg />
       <Confetti
+        width={width}
+        height={height}
         colors={colors}
         initialVelocityX={initialVelocityX}
         initialVelocityY={initialVelocityY}
