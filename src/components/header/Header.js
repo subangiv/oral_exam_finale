818cf6cf6c9e0ea2e629d1a7b9d95c313dd48ec2
rachelvo <rach0727@stud.kea.dk>
2020-11-23 16:16:24 +0100

    expanded navigation in header with react-responsive + .expanedMenuList

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import BurgerMenu from "../burgermenu/BurgerMenu";
+import { useMediaQuery } from "react-responsive";
 
 export default function Header(props) {
-  return (
-    <header className={styles.header}>
-      <h2 className={styles.polloName}>
-        POLLO<span>POLLO</span>
-      </h2>
-      <div id="actionBtns" className={styles.actionBtns}>
+  const isMobile = useMediaQuery({ query: "(max-width: 1023px)" });
+  const ActionButtons = () => {
+    return (
+      <div className={styles.actionBtns}>
         <button className="btn rounded btn-donate" children="Donate" />
         <button className="btn rounded btn-primary" children="Offer product" />
         <button
@@ -16,7 +15,40 @@ export default function Header(props) {
           children="Apply for product"
         />
       </div>
-      <BurgerMenu />
+    );
+  };
+  const Title = () => {
+    return (
+      <h2 className={styles.polloName}>
+        POLLO<span>POLLO</span>
+      </h2>
+    );
+  };
+  return (
+    <header className={styles.header}>
+      {isMobile ? (
+        <>
+          <Title />
+          <ActionButtons />
+          <BurgerMenu />
+        </>
+      ) : (
+        <>
+          <ul className={styles.expanedMenuList}>
+            <li>About</li>
+            <li>Donations</li>
+            <li>Applications</li>
+            <li>Products</li>
+            <li>
+              <Title />
+            </li>
+            <li>
+              <ActionButtons />
+            </li>
+            <li>Sign in</li>
+          </ul>
+        </>
+      )}
     </header>
   );
 }
