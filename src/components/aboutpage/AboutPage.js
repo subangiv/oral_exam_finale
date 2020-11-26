8671b962a69ddfc081de58f0dcb2ee8f5394b6bd
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-11-26 14:03:17 +0100

    added profiles

--- src/components/aboutpage/AboutPage.js
+++ src/components/aboutpage/AboutPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutBanner from "./AboutBanner";
 import AboutVid from "./AboutVid";
 import Articles from "./Articles";
+import Profiles from "./Profiles";
 
 export default function AboutPage() {
   return (
@@ -9,6 +10,7 @@ export default function AboutPage() {
       <AboutBanner />
       <AboutVid />
       <Articles />
+      <Profiles />
     </section>
   );
 }
---
+++ src/components/aboutpage/Profiles.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./AboutPage.module.scss";
+import data from "../json/profile.json";
+import avatar from "../../images/test-image.jpg";
+export default function Profiles() {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.profile__grid}>
+        {data.Profile.map((profile, i) => {
+          return (
+            <div key={i}>
+              <div className={styles.profile__div}>
+                <div className={styles.image__round}>
+                  <img src={avatar} alt="" />
+                </div>
+                <p>{profile.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
