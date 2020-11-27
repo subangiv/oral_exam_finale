c6fdc533a4e0fd84a32861fc48fd817532e5f2f7
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-11-27 11:07:16 +0100

    edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LandingPage from "./components/landingpage/LandingPage";
 import Success from "./components/payment/Success";
 
 function App() {
-  return <Success />;
+  return <LandingPage />;
 }
 
 export default App;
