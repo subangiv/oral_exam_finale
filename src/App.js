ebcd3f253facf8b36197064e3a007bcc59a8e198
rachelvo <rach0727@stud.kea.dk>
2020-11-30 13:05:30 +0100

    made helpPage with responsive media until 1440px

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import HelpPage from "./components/help_page/HelpPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      &emsp; &emsp;
+      <HelpPage />
       <Footer />
     </div>
   );
