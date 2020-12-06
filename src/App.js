17b2caed250a5f24120f026f11e1493aa4c1dd0b
rachelvo <rach0727@stud.kea.dk>
2020-12-06 11:34:23 +0100

    remove help_page component on this branch

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import HelpPage from "./components/help_page/HelpPage";
 import ContactPage from "./components/contact_page/ContactPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HelpPage />
       <ContactPage />
       <Footer />
     </div>
