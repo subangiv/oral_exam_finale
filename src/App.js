89d9ca42ca34f8b83931e31058097469562dde6a
rachelvo <rach0727@stud.kea.dk>
2020-12-06 11:36:49 +0100

    remove ContactPage comp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import HelpPage from "./components/help_page/HelpPage";
-import ContactPage from "./components/contact_page/ContactPage";
 import DonationPage from "./components/donation_page/DonationPage";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <Header />
       <DonationPage />
       <HelpPage />
-      <ContactPage />
       <Footer />
     </div>
   );
