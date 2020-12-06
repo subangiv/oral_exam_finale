d69ba81533865e44962541fc40f90a8767daddac
rachelvo <rach0727@stud.kea.dk>
2020-12-06 11:32:12 +0100

    remove donation component on this branch

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import HelpPage from "./components/help_page/HelpPage";
 import ContactPage from "./components/contact_page/ContactPage";
-import DonationPage from "./components/donation_page/DonationPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <DonationPage />
       <HelpPage />
       <ContactPage />
       <Footer />
