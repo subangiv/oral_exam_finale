61b432229f6c4daece873b496e43ede952281007
rachelvo <rach0727@stud.kea.dk>
2020-12-06 11:26:30 +0100

    making banner

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import HelpPage from "./components/help_page/HelpPage";
 import ContactPage from "./components/contact_page/ContactPage";
+import DonationPage from "./components/donation_page/DonationPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DonationPage />
       <HelpPage />
       <ContactPage />
       <Footer />
