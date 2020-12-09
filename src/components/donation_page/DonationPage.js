190aa93476b338beec65f5f8e2dd93cd3cfc9297
rachelvo <rach0727@stud.kea.dk>
2020-12-09 15:12:58 +0100

    done banner mobile version

--- src/components/donation_page/DonationPage.js
+++ src/components/donation_page/DonationPage.js
@@ -5,10 +5,21 @@ import DonationImage from "../../assets/images/donation-img-small.png";
 export default function DonationPage(props) {
   return (
     <main className={styles.donationPage}>
-      <div className={styles.donationBanner}>
-        <img src={DonationImage} alt="figure with computer" />
+      <div className={styles.donationBox}>
+        <img
+          src={DonationImage}
+          alt="donation page's banner"
+          className={styles.donationImg}
+        />
       </div>
       <h1 className="display-1">Donations</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur.
+      </p>
     </main>
   );
 }
