94c4141fae6d3e4c422b23333a1335f4c643d0a2
Subangi <suba0107@stud.kea.dk>
2020-12-11 12:16:44 +0100

    product page loading cards from data, styling and load more

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.scss";
 
 function ProductCard(props) {
 
+    console.log(props);
+const [detailsToggle, setDetailsToggle] = useState(false)
+const [showMore, setShowMore] = useState(false);
     return (
-     <li class="product-card-list-item">
-         <article class="styles.product-card-item">
-            <h2 class="product-card-title"></h2>
-            <h3>$5</h3>
-            <p class="product-card-"></p>
-            <img class="product-card-image" src="" alt=""/>
-            <img class="product-card-flag flag" src="" alt=""/>
-                <button>+</button>
-                <section>
-                    <h4>Details</h4>
-                    <p></p>
+     <li className={styles.productCardListItem}>
+         <article className={styles.productCardItem}>
+             <div className={styles.productCardWrapper}>
+                <img className={styles.productCardImage} src={"https://exampollopollo-e360.restdb.io/media/"+props.image[0]} alt=""/>
+                <section className={styles.productCardInfo}>
+                    <hgroup className={styles.productCardHeading}>
+                        <h2 className={"display-4 " + styles.productCardTitle }>{props.title}</h2>
+                        <h3>{"$" + props.price}</h3>
+                    </hgroup>
+                    <p className={styles.productCardDesc}>{props.description}</p>
                 </section>
+            </div>
+            <img className="product-card-flag flag" src="" alt=""/>
+            <section className={styles.productCardInteraction}>
+                <button onClick={() => {setDetailsToggle(!detailsToggle)}} className={styles.detailsButton}><b>+</b> Details</button>
+                <button className="btn btn-primary rounded">Apply</button>
+            </section>
+            {detailsToggle &&
+                <section className={styles.expandCollapse}>
+                    <ul className={styles.statistics}>
+                        <li>Recieved by applicants {props.recieved} times.</li>
+                        <li>Last recieved by an applicant at {props.lastDonation}</li>
+                    </ul>
+                    <a href="#" className={"primary-text"}>View producer's profile</a>
+                </section>
+            }
          </article>
      </li>
     );
--- src/components/products-page/ProductsPage.js
+++ src/components/products-page/ProductsPage.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import ProductCard from "../product-card/ProductCard";
 import styles from "./ProductsPage.module.scss";
 import text from "./static/product-page.json"
@@ -5,9 +6,9 @@ import "../product-card/ProductCard"
 
   const myCards = [
     {
-        productName: "Milk",
-        productDesc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
-        productImg: "milk.jpg",
+        name: "Milk",
+        desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        img: "milk.jpg",
         price: 5,
         country: "Venezuela, Bolivarian Republic of",
         landCode: "ve",
@@ -27,8 +28,9 @@ import "../product-card/ProductCard"
       }
     },
     {
-        productName: "cheese",
-        productDesc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        name: "Beef",
+        desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        img: "beef.jpg",
         price: 5,
         country: "Venezuela, Bolivarian Republic of",
         landCode: "ve",
@@ -47,19 +49,146 @@ import "../product-card/ProductCard"
             }
         }
     },
-  ];
+    {
+        name: "Beef",
+        desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        img: "beef.jpg",
+        price: 5,
+        country: "Venezuela, Bolivarian Republic of",
+        landCode: "ve",
+        id: 3,
+        statistics: {
+            lastDonation: "",
+            completedDonations: {
+                pastWeek:0,
+                pastMonth: 0,
+                allTime: 0,
+            },
+            pendingDonations: {
+              pastWeek:0,
+              pastMonth: 0,
+              allTime: 0
+            }
+        }
+    },
+    {
+        name: "Milk",
+        desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        img: "milk.jpg",
+        price: 5,
+        country: "Venezuela, Bolivarian Republic of",
+        landCode: "ve",
+        id: 4,
+        statistics: {
+            lastDonation: "",
+            completedDonations: {
+                pastWeek:0,
+                pastMonth: 0,
+                allTime: 0,
+            },
+            pendingDonations: {
+                pastWeek:0,
+                pastMonth: 0,
+                allTime: 0
+            }
+      }
+    }, 
+    {
+        name: "Beef",
+        desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.",
+        img: "beef.jpg",
+        price: 5,
+        country: "Venezuela, Bolivarian Republic of",
+        landCode: "ve",
+        id: 5,
+        statistics: {
+            lastDonation: "",
+            completedDonations: {
+                pastWeek:0,
+                pastMonth: 0,
+                allTime: 0,
+            },
+            pendingDonations: {
+                pastWeek:0,
+                pastMonth: 0,
+                allTime: 0
+            }
+      }
+    }
+];
   const cards = myCards.map((card) => <ProductCard key={card.id} {...card}/>);
+  const productsShown = 2;
+
 
 function ProductsPage(props) {
+    const [shownProducts, setShownProducts] = useState([]);
+    const [index, setIndex] = useState(0);
+    const [total, setTotal] = useState(0)
+    const [pending, setPending] = useState(true);
+
+
+    useEffect(() => {
+        // showMoreProducts([...myCards], index, productsShown);
+        if(shownProducts.length === 0) {
+        getData(showProducts, "?max="+productsShown + "&totals=true");
+        console.log(shownProducts);
+        }
+      }, []);
+
+    function showProducts(data) {
+        // console.log(newCards)
+        console.log(data)
+        setShownProducts([...shownProducts].concat(data.data));
+        setIndex(index+productsShown);
+        setTotal(data.totals.total);        
+    }
+
+    function loadProductsHandler(){
+        setPending(true);
+        getData(showProducts, "?skip=" + index + "&max=" + productsShown + "&totals=true")
+    };
+
+    function getData(callback, parameter) {
+        fetch("https://exampollopollo-e360.restdb.io/rest/products" + parameter, {
+            method: "get",
+            headers: {
+            "x-apikey": "5fc678a84af3f9656800d169",
+            "cache-control": "no-cache",
+          },
+        })
+          .then((e) => e.json())
+          .then((data) => {
+            console.log(data);
+            callback(data);
+          })
+          .then(() => {
+            setPending(false);
+          })
+      }
 
     return (
         <main>
-            <h1 className={"primary-text"}>Products</h1>
-            <img className={styles.filler}></img>
-            <p>{text.paragraph1}</p>
-            <p>{text.paragraph2}</p>
-            <ul>{cards}</ul>
-            <button>Load more...</button>
+            <article className={styles.page}>
+                <section className={styles.topSection}>
+                    <div className={styles.bannerWrapper}>
+                        <div className={styles.filler}>
+                        <img className={styles.banner} src={process.env.PUBLIC_URL + "products-images/products-img.png"}></img>
+                        </div>
+                    </div>
+                    <section className={styles.paragraphWrapper}>
+                        <h1 className={"primary-text display-2"}>Products</h1>
+                        <p className={styles.paragraph + " primary-text"}>{text.paragraph1}</p>
+                        <p className={styles.paragraph + " primary-text"}>{text.paragraph2}</p>
+                    </section>
+                </section>
+                <ul className={styles.productList}>{shownProducts.map((card) => <ProductCard key={card._id} {...card}/>)}</ul>
+                {pending &&
+                <p>Loading ...</p>}
+
+                {shownProducts.length !== total  && (
+                <button className="btn outlined rounded btn-primary" onClick={loadProductsHandler} >Load more...</button>
+                )}
+            </article>
         </main>
     );
   }
