8b6121bae2febc06d51320209da0d1a9c6fa4579
rachelvo <rach0727@stud.kea.dk>
2020-12-14 20:27:39 +0100

    link pages in burgermenu

--- src/components/burgermenu/BurgerMenu.js
+++ src/components/burgermenu/BurgerMenu.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import styles from "./BurgerMenu.module.scss";
 import headerclasses from "../header/Header.module.scss";
 import { ReactSVG } from "react-svg";
@@ -11,10 +12,14 @@ export default function BurgerMenu(props) {
   const bars = useRef(null);
 
   const [open, setOpen] = useState(false);
-  const clickOpenMenu = () => {
+  const toggleOpenMenu = (e) => {
     setOpen(!open);
+    animateBars();
+  };
 
-    if (open === false) {
+  const animateBars = () => {
+    console.log(!open); //true
+    if (!open) {
       //transform burgermenu icon
       bar1.current.style.transform = "rotate(135deg) translate(5px, -6px)";
       bar2.current.style.opacity = "0";
@@ -29,6 +34,13 @@ export default function BurgerMenu(props) {
     }
   };
 
+  const clickCloseMenu = (e) => {
+    e.stopPropagation();
+    console.log(!open);
+    setOpen(!open);
+    animateBars();
+  };
+
   const MenuList = ({ children, open }) => {
     const content = open && <>{children}</>;
     return content;
@@ -36,30 +48,77 @@ export default function BurgerMenu(props) {
 
   return (
     <nav className={styles.navi}>
-      <div
-        ref={bars}
-        className={styles.bars}
-        onClick={clickOpenMenu}
-        open={open}
-      >
+      <button ref={bars} className={styles.bars} onClick={toggleOpenMenu}>
         <span ref={bar1} className={styles.bar}></span>
         <span ref={bar2} className={styles.bar}></span>
         <span ref={bar3} className={styles.bar}></span>
-      </div>
+      </button>
       <MenuList open={open}>
         {open === true && (
           <div className={styles.menuList} open={open}>
             <ul id="menuList">
-              <li>Home</li>
-              <li>About</li>
-              <li>Donations</li>
-              <li>Applications</li>
-              <li>Products</li>
+              <li>
+                <Link className="primary-text" to="/" onClick={clickCloseMenu}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/about"
+                  onClick={clickCloseMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/donations"
+                  onClick={clickCloseMenu}
+                >
+                  Donations
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/applications"
+                  onClick={clickCloseMenu}
+                >
+                  Applications
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/products"
+                  onClick={clickCloseMenu}
+                >
+                  Products
+                </Link>
+              </li>
               <li className={styles.separateBar}>
                 <hr />
               </li>
-              <li>Sign in</li>
-              <li>Register</li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/sign-in"
+                  onClick={clickCloseMenu}
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="primary-text"
+                  to="/sign-up"
+                  onClick={clickCloseMenu}
+                >
+                  Sign up
+                </Link>
+              </li>
             </ul>
             <div id="polloPolloLogo" className={styles.logoWrapper}>
               <ReactSVG
