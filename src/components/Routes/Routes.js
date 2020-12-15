95feb6f9b029afa2350a2ed9eeaa864ad2d791d5
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-12-15 01:34:21 +0100

    edited responsiveness for account

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -41,6 +41,9 @@ const Routes = (props) => (
       <Route path="/">
         <LandingPage />
       </Route>
+      <Route>
+        <Success />
+      </Route>
     </Switch>
     <Footer />
   </Router>
