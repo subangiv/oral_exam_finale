ec55c4c7ca0f7a6585f9937e65f2b2e96416d66b
Subangi <suba0107@stud.kea.dk>
2020-12-17 00:14:51 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
 import './App.css';
-<<<<<<< HEAD
 import React from "react";
-=======
-import InputField from './components/input-field/InputField'
-import ProductsPage from './components/products-page/ProductsPage'
-import ApplicationsPage from './components/applications-page/ApplicationsPage'
-import React, { useEffect, useState } from "react";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import SignUp from "./components/account/SignUp";
-import SignIn from "./components/account/SignIn";
-import LandingPage from "./components/landingpage/LandingPage";
-import AboutPage from "./components/aboutpage/AboutPage"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
->>>>>>> subangi
 import Routes from './components/Routes/Routes';
 
 function App() {
