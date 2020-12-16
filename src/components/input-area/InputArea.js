320958a8d35cd79dd8a3f0c0bd736cdd3c925971
rachelvo <rach0727@stud.kea.dk>
2020-12-16 18:43:25 +0100

    small fixes on custom inputs and select + remove default style

--- src/components/input-area/InputArea.js
+++ src/components/input-area/InputArea.js
@@ -12,10 +12,16 @@ function InputArea(props) {
           cols={props.cols}
           rows={props.rows}
           onChange={props.onChange}
+          onInput={props.onInput}
+          onKeyDown={props.onKeyDown}
+          onKeyUp={props.onKeyUp}
+          onBlur={props.onBlur}
+          onInvalid={props.onInvalid}
           required={props.required}
         />
         <label htmlFor={props.labelFor} children={props.label} />
       </div>
+      {props.children}
     </div>
   );
 }
