c144de0fe6734834970516c2a1ddab7b4d1f84f3
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-12-16 21:34:53 +0100

    pulled from development

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import './App.css';
-<<<<<<< HEAD
+import "./App.css";
 import React from "react";
-=======
-import InputField from './components/input-field/InputField'
-import ProductsPage from './components/products-page/ProductsPage'
-import ApplicationsPage from './components/applications-page/ApplicationsPage'
-import React, { useEffect, useState } from "react";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import SignUp from "./components/account/SignUp";
-import SignIn from "./components/account/SignIn";
-import LandingPage from "./components/landingpage/LandingPage";
-import AboutPage from "./components/aboutpage/AboutPage"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
->>>>>>> subangi
-import Routes from './components/Routes/Routes';
+import Routes from "./components/Routes/Routes";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -24,20 +10,18 @@ function App() {
     logInHandler();
   }, []);
 
-  console.log(isLoggedIn)
+  console.log(isLoggedIn);
   function logInHandler() {
     const user = localStorage.getItem("user");
 
-    if(user === null) {
+    if (user === null) {
       setIsLoggedIn(false);
     } else {
       setIsLoggedIn(true);
     }
   }
 
-  return (
-    <Routes isLoggedIn={isLoggedIn}/>
-  );
+  return <Routes isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
